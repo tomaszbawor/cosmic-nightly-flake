5407ffed76a3cf11a05b78f909d7df8617f2e523
Charlie Wilkin <charlie@busyboredom.com>
2025-08-21 13:37:24 -0400

    Implement cosmic nightly flake

---
+++ README.md
@@ -0,0 +1,92 @@
+# COSMIC Nightly Flake for NixOS
+
+This repository provides a Nix flake that overlays the official COSMIC packages in `nixpkgs-unstable` with the latest nightly builds directly from the Pop!_OS GitHub repositories.
+
+This flake is for users who want to test the cutting-edge, pre-release versions of the COSMIC desktop environment on their NixOS systems (what's stability
+good for, anyway?).
+
+## Disclaimer and Contributions
+
+This is a personal side project. I make no guarantees that it will work correctly or that it will be consistently updated. The primary goal is to provide a convenient way to test the latest COSMIC builds, but things may break and I have limited time allocated to keep it working.
+
+**Contributions are welcome!** If you notice a package is missing or would like to help maintain the flake (e.g., by adding CI to automate hash updates), please feel free to open a Pull Request.
+
+## Prerequisites
+
+Before you begin, ensure that you have flakes enabled in your NixOS configuration.
+
+## How to Use
+
+Getting started involves editing your NixOS flake configuration to use the nightly COSMIC source and then enabling the desktop environment.
+
+### 1. Add the Input and Apply the Overlay
+
+In your system's `flake.nix` (e.g., in `/etc/nixos/flake.nix`), you need to perform two actions: add this repository to your `inputs` and then apply its `overlay` to your Nixpkgs configuration.
+
+```nix
+# /etc/nixos/flake.nix
+{
+  description = "My NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Add the COSMIC nightly flake
+    cosmic-nightly = {
+      url = "github:busyboredom/cosmic-nightly-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, cosmic-nightly, ... }@inputs: {
+    nixosConfigurations.your-hostname = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Apply the overlay here
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [ cosmic-nightly.overlay ];
+        })
+
+        ./configuration.nix
+      ];
+    };
+  };
+}
+````
+
+### 2\. Enable the COSMIC Desktop
+
+In your `configuration.nix`, enable the COSMIC desktop environment and a display manager. The `cosmic-greeter` is recommended for the most integrated experience.
+
+```nix
+# /etc/nixos/configuration.nix
+{ config, pkgs, ... }:
+
+{
+  # ... other system settings
+
+  # Enable the COSMIC Desktop Environment
+  services.desktopManager.cosmic.enable = true;
+
+  # Enable a display manager. cosmic-greeter is recommended.
+  services.displayManager.cosmic-greeter.enable = true;
+}
+```
+
+### 3\. Rebuild Your System
+
+After saving your changes, apply the new configuration:
+
+```sh
+sudo nixos-rebuild switch --flake /etc/nixos#your-hostname
+```
+
+## Updating
+
+```sh
+nix flake update /etc/nixos
+```
+
+Then, run the `nixos-rebuild switch` command again to apply the updates.
---
+++ flake.nix
@@ -0,0 +1,343 @@
+{
+  description = "A flake for nightly builds of the COSMIC desktop environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # COSMIC DE Components
+    cosmic-applets-src = {
+      url = "github:pop-os/cosmic-applets";
+      flake = false;
+    };
+    cosmic-applibrary-src = {
+      url = "github:pop-os/cosmic-applibrary";
+      flake = false;
+    };
+    cosmic-bg-src = {
+      url = "github:pop-os/cosmic-bg";
+      flake = false;
+    };
+    cosmic-comp-src = {
+      url = "github:pop-os/cosmic-comp";
+      flake = false;
+    };
+    cosmic-edit-src = {
+      url = "github:pop-os/cosmic-edit";
+      flake = false;
+    };
+    cosmic-files-src = {
+      url = "github:pop-os/cosmic-files";
+      flake = false;
+    };
+    cosmic-greeter-src = {
+      url = "github:pop-os/cosmic-greeter";
+      flake = false;
+    };
+    cosmic-icons-src = {
+      url = "github:pop-os/cosmic-icons";
+      flake = false;
+    };
+    cosmic-idle-src = {
+      url = "github:pop-os/cosmic-idle";
+      flake = false;
+    };
+    cosmic-launcher-src = {
+      url = "github:pop-os/cosmic-launcher";
+      flake = false;
+    };
+    cosmic-notifications-src = {
+      url = "github:pop-os/cosmic-notifications";
+      flake = false;
+    };
+    cosmic-osd-src = {
+      url = "github:pop-os/cosmic-osd";
+      flake = false;
+    };
+    cosmic-panel-src = {
+      url = "github:pop-os/cosmic-panel";
+      flake = false;
+    };
+    cosmic-player-src = {
+      url = "github:pop-os/cosmic-player";
+      flake = false;
+    };
+    cosmic-protocols-src = {
+      url = "github:pop-os/cosmic-protocols";
+      flake = false;
+    };
+    cosmic-randr-src = {
+      url = "github:pop-os/cosmic-randr";
+      flake = false;
+    };
+    cosmic-screenshot-src = {
+      url = "github:pop-os/cosmic-screenshot";
+      flake = false;
+    };
+    cosmic-session-src = {
+      url = "github:pop-os/cosmic-session";
+      flake = false;
+    };
+    cosmic-settings-src = {
+      url = "github:pop-os/cosmic-settings";
+      flake = false;
+    };
+    cosmic-settings-daemon-src = {
+      url = "github:pop-os/cosmic-settings-daemon";
+      flake = false;
+    };
+    cosmic-store-src = {
+      url = "github:pop-os/cosmic-store";
+      flake = false;
+    };
+    cosmic-term-src = {
+      url = "github:pop-os/cosmic-term";
+      flake = false;
+    };
+    cosmic-text-src = {
+      url = "github:pop-os/cosmic-text";
+      flake = false;
+    };
+    cosmic-theme-editor-src = {
+      url = "github:pop-os/cosmic-theme-editor";
+      flake = false;
+    };
+    cosmic-wallpapers-src = {
+      url = "github:pop-os/cosmic-wallpapers";
+      flake = false;
+    };
+    cosmic-workspaces-epoch-src = {
+      url = "github:pop-os/cosmic-workspaces-epoch";
+      flake = false;
+    };
+    libcosmic-src = {
+      url = "github:pop-os/libcosmic";
+      flake = false;
+    };
+    xdg-desktop-portal-cosmic-src = {
+      url = "github:pop-os/xdg-desktop-portal-cosmic";
+      flake = false;
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      overlay =
+        final: prev:
+        let
+          pkgs = prev; # Use the package set being overlaid
+          lib = pkgs.lib;
+
+          # A wrapper script to intercept a problematic git command used by vergen.
+          git-wrapper = pkgs.writeShellScriptBin "git" ''
+            #!${pkgs.runtimeShell}
+            # When vergen runs its specific check to see if it's in a git repo...
+            if [ "$*" = "rev-parse --is-inside-work-tree" ]; then
+              # ...we lie and tell it "yes" so the build can continue.
+              echo "true"
+              exit 0
+            fi
+            # For all other git commands, we execute the real git program.
+            exec ${pkgs.git}/bin/git "$@"
+          '';
+
+          # A helper function to create an override for a single package.
+          # It takes the nixpkgs derivation name and the corresponding flake input.
+          mkCosmicOverride =
+            {
+              pkgName,
+              flakeInput,
+              pkgHash ? "",
+              extraBuildInputs ? [ ],
+              extraNativeBuildInputs ? [ ],
+              patches ? null,
+              extraEnv ? { },
+            }:
+            prev.${pkgName}.overrideAttrs (
+              oldAttrs:
+              {
+                # This is the core of the overlay: replace the source attribute
+                # with the fetched source from the dedicated flake input.
+                src = flakeInput;
+
+                env = {
+                  VERGEN_GIT_SHA = flakeInput.rev;
+                }
+                // extraEnv;
+
+                cargoHash = pkgHash;
+                buildInputs = (oldAttrs.buildInputs or [ ]) ++ extraBuildInputs;
+                nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ extraNativeBuildInputs;
+
+                cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                  inherit (final.${pkgName}) pname src version;
+                  hash = final.${pkgName}.cargoHash;
+                };
+
+                version = "git-${builtins.substring 0 8 flakeInput.rev}";
+              }
+              // lib.optionalAttrs (patches != null) { inherit patches; }
+            );
+
+          mkCosmicFetchOverride =
+            {
+              pkgName,
+              flakeInput,
+            }:
+            prev.${pkgName}.overrideAttrs (oldAttrs: {
+              src = flakeInput;
+              version = "git-${builtins.substring 0 8 flakeInput.rev}";
+            });
+        in
+        {
+          # Apply the override to each COSMIC component.
+          cosmic-applets = mkCosmicOverride {
+            pkgName = "cosmic-applets";
+            flakeInput = inputs.cosmic-applets-src;
+            pkgHash = "sha256-v4hlNZXmaX3kipbhFRo1gG7BjUIeumv1F0Z/8CYoSyc=";
+            extraBuildInputs = [
+              pkgs.pipewire
+              pkgs.llvmPackages.libclang
+            ];
+            extraNativeBuildInputs = [ pkgs.rustPlatform.bindgenHook ];
+            extraEnv = {
+              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+            };
+          };
+          cosmic-applibrary = mkCosmicOverride {
+            pkgName = "cosmic-applibrary";
+            flakeInput = inputs.cosmic-applibrary-src;
+            pkgHash = "sha256-Jw8XvrMMIGzioMxNUWXV+hfu6fGu0vpvS7dAmJwo7SU=";
+          };
+          cosmic-bg = mkCosmicOverride {
+            pkgName = "cosmic-bg";
+            flakeInput = inputs.cosmic-bg-src;
+            pkgHash = "sha256-iCQjPZH3CN73R6PmFRndLcPZGQfxeaPSYPZgbGofKkM=";
+          };
+          cosmic-comp = mkCosmicOverride {
+            pkgName = "cosmic-comp";
+            flakeInput = inputs.cosmic-comp-src;
+            pkgHash = "sha256-XyiPpYVqk9y1V+0R0zIHXxLuao8qS8o8ZGTqp8+32PE=";
+          };
+          cosmic-edit = mkCosmicOverride {
+            pkgName = "cosmic-edit";
+            flakeInput = inputs.cosmic-edit-src;
+            pkgHash = "sha256-DoAPF06Jf2dQCEUPPYPHQSJ9UIBdMoogS/A/n8JyzRM=";
+            extraBuildInputs = [ pkgs.glib ];
+            extraNativeBuildInputs = [ git-wrapper ];
+          };
+          cosmic-files = mkCosmicOverride {
+            pkgName = "cosmic-files";
+            flakeInput = inputs.cosmic-files-src;
+            pkgHash = "sha256-TDXo0PsDLIBewAasBK82VsG1O0lPqY6g3dBRFsGzF6A=";
+          };
+          cosmic-greeter = mkCosmicOverride {
+            pkgName = "cosmic-greeter";
+            flakeInput = inputs.cosmic-greeter-src;
+            pkgHash = "sha256-X/tSofi4aNtA5MeWCy03Tnnz3AxIF8MCZ7ofeMSWNCc=";
+          };
+          cosmic-icons = mkCosmicFetchOverride {
+            pkgName = "cosmic-icons";
+            flakeInput = inputs.cosmic-icons-src;
+          };
+          cosmic-idle = mkCosmicOverride {
+            pkgName = "cosmic-idle";
+            flakeInput = inputs.cosmic-idle-src;
+            pkgHash = "sha256-iFR0kFyzawlXrWItzFQbG/tKGd3Snwk/0LYkPzCkJUQ=";
+          };
+          cosmic-launcher = mkCosmicOverride {
+            pkgName = "cosmic-launcher";
+            flakeInput = inputs.cosmic-launcher-src;
+            pkgHash = "sha256-57rkCufJPWm844/iMIfULfaGR9770q8VgZgnqCM57Zg=";
+          };
+          cosmic-notifications = mkCosmicOverride {
+            pkgName = "cosmic-notifications";
+            flakeInput = inputs.cosmic-notifications-src;
+            pkgHash = "sha256-3rBbjAVdpNKYBHOrI/Zsb4Q5J9Xx4ddeCpzsUK51mns=";
+          };
+          cosmic-osd = mkCosmicOverride {
+            pkgName = "cosmic-osd";
+            flakeInput = inputs.cosmic-osd-src;
+            pkgHash = "sha256-C+R2XgWtErznv6TQZ9eke9/ZNiRUVparP5yHu9442wA=";
+          };
+          cosmic-panel = mkCosmicOverride {
+            pkgName = "cosmic-panel";
+            flakeInput = inputs.cosmic-panel-src;
+            pkgHash = "sha256-VlEbbQTAX05zJYURZym4bBhCtbQ85ujvqLMQNHSz23o=";
+          };
+          cosmic-player = mkCosmicOverride {
+            pkgName = "cosmic-player";
+            flakeInput = inputs.cosmic-player-src;
+          };
+          cosmic-protocols = mkCosmicOverride {
+            pkgName = "cosmic-protocols";
+            flakeInput = inputs.cosmic-protocols-src;
+            pkgHash = "";
+          };
+          cosmic-randr = mkCosmicOverride {
+            pkgName = "cosmic-randr";
+            flakeInput = inputs.cosmic-randr-src;
+            pkgHash = "sha256-tkmBthh+nM3Mb9WoSjxMbx3t0NTf6lv91TwEwEANS6U=";
+          };
+          cosmic-screenshot = mkCosmicOverride {
+            pkgName = "cosmic-screenshot";
+            flakeInput = inputs.cosmic-screenshot-src;
+            pkgHash = "sha256-1r0Uwcf4kpHCgWqrUYZELsVXGDzbtbmu/WFeX53fBiQ=";
+          };
+          cosmic-session = mkCosmicOverride {
+            pkgName = "cosmic-session";
+            flakeInput = inputs.cosmic-session-src;
+            pkgHash = "sha256-4leO8F32O4E+fqpR0/Nj5wBcY0N00J/JdsYnPwPCWps=";
+          };
+          cosmic-settings = mkCosmicOverride {
+            pkgName = "cosmic-settings";
+            flakeInput = inputs.cosmic-settings-src;
+            pkgHash = "sha256-lPAtrV4ZrbhlC4P0TA/PuNc/LeCiflru6MYxYYN2qH8=";
+          };
+          cosmic-settings-daemon = mkCosmicOverride {
+            pkgName = "cosmic-settings-daemon";
+            flakeInput = inputs.cosmic-settings-daemon-src;
+            pkgHash = "sha256-9BeC0Y29NOMoEJHKLV3aRHZQbglbLnnTH4uS3h129iw=";
+            extraBuildInputs = [ pkgs.openssl ];
+          };
+          cosmic-store = mkCosmicOverride {
+            pkgName = "cosmic-store";
+            flakeInput = inputs.cosmic-store-src;
+            pkgHash = "";
+          };
+          cosmic-term = mkCosmicOverride {
+            pkgName = "cosmic-term";
+            flakeInput = inputs.cosmic-term-src;
+          };
+          cosmic-text = mkCosmicOverride {
+            pkgName = "cosmic-text";
+            flakeInput = inputs.cosmic-text-src;
+            pkgHash = "";
+          };
+          cosmic-theme-editor = mkCosmicOverride {
+            pkgName = "cosmic-theme-editor";
+            flakeInput = inputs.cosmic-theme-editor-src;
+            pkgHash = "";
+          };
+          cosmic-wallpapers = mkCosmicFetchOverride {
+            pkgName = "cosmic-wallpapers";
+            flakeInput = inputs.cosmic-wallpapers-src;
+          };
+          cosmic-workspaces-epoch = mkCosmicOverride {
+            pkgName = "cosmic-workspaces-epoch";
+            flakeInput = inputs.cosmic-workspaces-epoch-src;
+            pkgHash = "sha256-wFX5EReAnZ7ymXYfMfiZU1MeUUCcOKEkWdSeyGHEuKg=";
+          };
+          libcosmic = mkCosmicOverride {
+            pkgName = "libcosmic";
+            flakeInput = inputs.libcosmic-src;
+            pkgHash = "";
+          };
+          xdg-desktop-portal-cosmic = mkCosmicOverride {
+            pkgName = "xdg-desktop-portal-cosmic";
+            flakeInput = inputs.xdg-desktop-portal-cosmic-src;
+            pkgHash = "sha256-NQoqbfNEMWowo2KxdgKqTbn/BDgv218NFCCGYR9OAO0=";
+          };
+        };
+    };
+}
